7a471dbf627d7fabb9fe4d62e6ed5a6b05c050c0
Ankit <mr.pandeyankit14@gmail.com>
2024-01-15 22:19:14 +0530

    Deployed

---
+++ products.js
@@ -0,0 +1,12 @@
+const productList = [
+    {id: 1, name:   "Shoe"   , price:500},
+    {id: 2, name: "Jeans", price:400},
+    {id: 3, name: "Shirt", price:450},
+    {id: 4, name: "Jacket"   , price:1000},
+    {id: 5, name: "T-Shirt"   , price:800}
+    
+];
+
+
+
+
